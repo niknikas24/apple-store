03331933efe0ab8f0d8565a0cee7d00e8894f52c
niknikas24 <kvadrolis@gmail.com>
2025-06-18 21:08:26 +0300

    Create Home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <BannerTop />
+    <SmallerBanners />
+    <Categories />
+    <ProductsHome />
+    <BannersPopular />
+    <DiscountProducts />
+    <BannerBottom />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BannerTop from '@/components/BannerTop.vue';
+import SmallerBanners from '@/components/SmallerBanners.vue';
+import Categories from '@/components/Categories.vue';
+import ProductsHome from '@/components/ProductsHome.vue';
+import BannersPopular from '@/components/BannersPopular.vue';
+import DiscountProducts from '@/components/DiscountProducts.vue';
+import BannerBottom from '@/components/BannerBottom.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: { BannerTop, SmallerBanners, BannerBottom, Categories, ProductsHome, BannersPopular, DiscountProducts },
+});
+</script>
+
+<style scoped>
+</style>
